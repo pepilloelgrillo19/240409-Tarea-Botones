90570298b7c2afbab4782eb4af8ea933273bb095
pepilloelgrillo19 <157523378+pepilloelgrillo19@users.noreply.github.com>
2024-04-09 19:27:25 +0200

    Initial Commit

---
+++ 240409 Tarea Botones/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  240409 Tarea Botones
+//
+//  Created by Luis on 9/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 240409 Tarea Botones/_40409_Tarea_BotonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  _40409_Tarea_BotonesApp.swift
+//  240409 Tarea Botones
+//
+//  Created by Luis on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct _40409_Tarea_BotonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
